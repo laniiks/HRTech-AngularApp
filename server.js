2f36f4299f5bbf39fc95431e8144b7d33c37768f
Котов Николай Алексеевич <n.kotov@solarl.ru>
2021-12-04 14:17:05 +0300

    +

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/HRTech/index.html'));});
 app.listen(process.env.PORT || 8080);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
